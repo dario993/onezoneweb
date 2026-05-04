b8ca96af2a86a0af9abcc22a07544a0e87d3051f
Dario993 <dario.sgamba@gmail.com>
2026-05-04 17:42:17 +0200

    aggiornata api anche su environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiEndpoint: "https://onezone.brokerstar.biz/api/v3",
-  automationApiEndpoint: "https://nonenforcedly-disjointed-marylouise.ngrok-free.dev",
+  automationApiEndpoint: "https://api-car-scraping.onezone.ch",
   automationAdminApiKey: "valeman",
 };
